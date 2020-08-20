41adc41105b88edba5ac21e2111ce8b9723ea84b
Greg Doyle <caligd45@gmail.com>
2020-08-20 14:21:06 -0600

    debugging

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"; 
 
 mongoose.connect(MONGODB_URI)
 
