9fe7eb1f37dc7e08836b623e5590a11c98363a8f
Greg Doyle <caligd45@gmail.com>
2020-08-20 14:16:41 -0600

    debugging

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
+mongoose.connect(MONGODB_URI)
+
 // routes
 app.use(require("./routes/api.js"));
 
